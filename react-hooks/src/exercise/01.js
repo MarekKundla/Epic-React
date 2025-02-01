71f4c32fbd6246f14e58cfba63155a0c70e8f8fb
Marek Kundla <Marek.Kundla@gmail.com>
2025-02-01 15:48:26 +0100

    react-hooks exercise 4

--- react-hooks/src/exercise/01.js
+++ react-hooks/src/exercise/01.js
@@ -29,7 +29,7 @@ function Greeting() {
 
 // Extra Credit 1
 
-// function Greeting({initialName}) {
+// function Greeting({initialName = ''}) {
 
 //     const [name, setName] = React.useState(initialName)
 
--- react-hooks/src/exercise/02.js
+++ react-hooks/src/exercise/02.js
@@ -65,14 +65,14 @@ function Greeting({ initialName = '' }) {
 
 // Extra Credit 4
 
-// function useLocalStorageState (key, defaultValue = ') {
+// function useLocalStorageState (key, defaultValue = '') {
 //     const [state, setState] = React.useState(() => {
-//     const valueInLocalStorage = window.localStorage.getItem(key)
-//     if (valueInLocalStorage) {
-//         return JSON.parse(valueInLocalStorage)
-//     }
-//     return defaultValue
-// })
+//         const valueInLocalStorage = window.localStorage.getItem(key)
+//         if (valueInLocalStorage) {
+//             return JSON.parse(valueInLocalStorage)
+//         }
+//         return defaultValue
+//     })
 
 //     React.useEffect(() => {
 //         window.localStorage.setItem(key, JSON.stringify(state))
